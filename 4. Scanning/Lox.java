d31e06c09123fd6e8ee43ffa9bfff52ae7ac2c5e
reality_user <wow@reality.com>
2023-03-04 14:23:26 -0600

    Added runFile, runPrompt, and run

--- 4. Scanning/Lox.java
+++ 4. Scanning/Lox.java
@@ -14,11 +14,41 @@ public class Lox {
 			System.out.println("Usage: jlox [script]");
 			System.exit(64); 
 		} else if (args.length == 1) {
-			//runFile(args[0]);
-			System.out.println("passed 1 argument");
+			runFile(args[0]);
+			//System.out.println("passed 1 argument");
 		} else {
-			//runPrompt();
-			System.out.println("passed no arguments");
+			runPrompt();
+			//System.out.println("passed no arguments");
 		}
+	}
+
+	private static void runFile(String path) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get(path)); //throws IOException if "path" does not refer to real filename
+		run(new String(bytes, Charset.defaultCharset()));
+	}
+
+	private static void runPrompt() throws IOException {
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+
+		for (;;) {
+			System.out.print("> ");
+			String line = reader.readLine();
+			if (line == null) break;
+			run(line);
+		}
+	}
+
+	private static void run(String source) {
+		//Scanner scanner = new Scanner(source);
+		//List<Token> tokens = scanner.scanTokens();
+
+		////for now, just print the tokens.
+		//for (Token token : tokens) {
+		//	System.out.println(token);
+		//}
+
+		System.out.println("PROGRAM IS RUNNED");
+
 	}
 }
