a6d1e65f27d0d3b1cdb67cf3c77881d1e8b15a35
reality_user <wow@reality.com>
2023-03-04 22:17:20 -0600

    Continued transcribing operators in scanner

--- 4. Scanning/Scanner.java
+++ 4. Scanning/Scanner.java
@@ -32,6 +32,7 @@ class Scanner {
 	private void scanToken() {
 		char c = advance();
 		switch(c) {
+			//single char operators
 			case '(': addToken(LEFT_PAREN); break;
 			case ')': addTokenRIGHT_PAREN(); break;
 			case '{': addToken(LEFT_BRACE); break;
@@ -42,11 +43,79 @@ class Scanner {
 			case '+': addTokenPLUS(); break;
 			case ';': addToken(SEMICOLON); break;
 			case '*': addToken(STAR); break;
+			case '!':
+				  addToken(match('=') ? BANG_EQUAL : BANG);
+				  break;
+			case '=':
+				  addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+				  break;
+			case '<':
+				  addToken(match('=') ? LESS_EQUAL : LESS);
+				  break;
+			case '>':
+				  addToken(match('=') ? GREATER_EQUAL : GREATER);
+				  break;
+			case '/':
+				  if (match('/')) {
+					  //A comment goes until the end of the line.
+					  while (peek() != '\n' && !isAtEnd()) advance();
+				  } else {
+					  addToken(SLASH);
+				  }
+				  break;
+
+				 
+			//whitespace
+			case ' ':
+			case '\r':
+			case '\t':
+				  //ignore whitespace
+				  break;
+			case '\n':
+				  line++;
+				  break;
+
+			//literals
+			case '"': string(); break;
+
 			default:
 				  Lox.error(line, "Unexpected character."); //why don't we output c here?
 									    //note: I'm sligtly confused about the relationship between this file and the Lox class...
 				  break;
 		}
+	}
+
+	private void string() {
+		while (peek() != '"' && !isAtEnd()) {
+			if (peek() == '\n') line++;
+			advance();
+		}
+
+		if (isAtEnd()) {
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+
+		//the closing ".
+		advance();
+
+		//trim the surrounding quotes
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
+	}
+
+	private boolean match(char expected) {
+		if (isAtEnd()) return false;
+		if (source.charAt(current) != expected) return false;
+
+		//else, matched expected
+		current++;
+		return true;
+	}
+
+	private char peek() {
+		if (isAtEnd()) return '\0';
+		return source.charAt(current);
 	}
 
 	private boolean isAtEnd() {
