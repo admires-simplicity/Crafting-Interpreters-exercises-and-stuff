76de9133fd475be7fae5690b8f55da64cfc9c0d0
reality_user <wow@reality.com>
2023-06-01 10:52:37 -0500

    finished ch. 5

---
+++ 5. Representing Code/challenges/challenge 2/challenge.hs
@@ -0,0 +1,21 @@
+-- OOP:
+--  class has defined functionality
+--  add new class (type) implementing analogous functionality
+-- 
+--  new type, same semantics
+-- 
+--  visitor pattern: allows us to add functionality to set of types at once
+-- 
+-- FUNC:
+--  functions defined on set of types
+--  add new function on same set of types
+--  
+--  new function, same types
+--  
+--  ?? pattern: allows us to add set of semantic functions to one type....
+--  
+--  bundle operations on a type and let you define new types (implementing same semantic functions) easily...
+
+
+
+-- I tried to figure this out but honestly I don't understand haskell well enough
