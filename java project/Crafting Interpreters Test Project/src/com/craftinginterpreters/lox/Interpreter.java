ce47bdaf76d92ac9b1bb8e191e78e790c904b675
reality_user <wow@reality.com>
2023-06-22 12:09:50 -0500

    finished reading/copying ch. 8

--- java project/Crafting Interpreters Test Project/src/com/craftinginterpreters/lox/Interpreter.java
+++ java project/Crafting Interpreters Test Project/src/com/craftinginterpreters/lox/Interpreter.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void> {
+  private Environment environment = new Environment();
 
   void interpret(List<Stmt> statements) {
     try {
@@ -23,6 +24,25 @@ class Interpreter implements Expr.Visitor<Object>,
     stmt.accept(this);
   }
 
+  void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
+  }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
   @Override
   public Void visitExpressionStmt(Stmt.Expression stmt) {
     evaluate(stmt.expression);
@@ -34,6 +54,24 @@ class Interpreter implements Expr.Visitor<Object>,
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
   }
 
   @Override
@@ -113,6 +151,11 @@ class Interpreter implements Expr.Visitor<Object>,
 
     // Unreachable
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private void checkNumberOperand(Token operator, Object operand) {
