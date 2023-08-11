8f22e041453260815e7417d0a89d402dbe3d9aa6
reality_user <jc3124567@gmail.com>
2023-08-11 00:22:05 -0500

    finished reading chapter 15 and backed up

---
+++ 15. A Virtual Machine/src/vm.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "debug.h"
+#include "vm.h"
+
+VM vm;
+
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
+void initVM() {
+  resetStack();
+}
+
+void freeVM() {
+
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++) //I think this is equivalent to (*(vm.ip++)) but not ((*vm.ip)++) even though * [dereference operator] and ++ are supposed to technically have equal precedence.
+                               //actually, according to https://stackoverflow.com/a/859795, what is happening is that * and ++ are both applied to *the pointer*, since they have equal precedence..... however the comments say this is wrong and that ++ is actually applied first.
+                               //according to https://godbolt.org/z/fh4K1z7qa the * and ++ operators do actually seem to sort of be applied at the same time. and I am right that (*p++) is equivalent to (*(p++)) and not ((*p)++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false)
+
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk,
+      (int)(vm.ip - vm.chunk->code));
+#endif
+
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        push(constant);
+        break;
+      }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NEGATE:   push(-pop()); break;
+      case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
+        return INTERPRET_OK;
+      }
+    }
+  }
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
+}
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
